31b0d05289c23f22061a7e58b8debd8bdeb8072b
ZonaphatINET <s6506022420011@email.kmutnb.ac.th>
2025-03-26 18:11:40 +0700

    up

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from pymongo import MongoClient
 import math
 from bson import json_util  # เพิ่ม import นี้ที่บรรทัดบน
-import json
+import json 
 
 app = Flask(__name__)
 
